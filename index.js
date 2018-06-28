6af46cf15f053c0eb8296300ec5e3eb7d7985af2
jenniferlong8 <jlong8@bu.edu>
2018-06-28 19:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
+
 console.log("Name:")
 console.log(name)
 
